baba3a4e97267f40f713f02779b8e45e45b8f771
Youssef Shoaib <canonballt@gmail.com>
2026-08-29 22:43:54 +0100

    Update foojay-resolver-convention plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 }
 
 plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
   id("com.gradleup.nmcp.settings") version "1.6.1"
   kotlin("compiler.plugin.devkit") version "0.0.3-dev-66e2b55"
 }
